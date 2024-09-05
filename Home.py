bd82ce292ee2e49a85469484538e1350bf168466
likeRudin <likeRudin@gmail.com>
2024-09-05 23:27:25 +0900

    feat: create home page

---
+++ Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Round-the-Clock Aide Home",
+    page_icon='🧟'
+)
+
+st.markdown(
+    """
+# Welcome to Unsleeping Aide's Office
+    """
+)
